10e5647a59f314bd598bc489888b82600e4685fe
Stuskoski <Stuskoski@yahoo.com>
2016-02-18 12:50:38 -0600

    working on another scene

--- src/views/StartingGameInfoView.java
+++ src/views/StartingGameInfoView.java
@@ -1,16 +1,19 @@
 package views;
 
-import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import main.Main;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -21,34 +24,26 @@ import java.util.TimerTask;
  */
 public class StartingGameInfoView {
     public static Scene startingGameInfoView;
-    public static Canvas canvas = new Canvas(Main.getPrimaryStage().getWidth()-200, Main.getPrimaryStage().getHeight()-200);
+    public static Canvas canvas = new Canvas(Main.getPrimaryStage().getWidth(), Main.getPrimaryStage().getHeight()-30);
     public static GraphicsContext gc = canvas.getGraphicsContext2D();
     public static int x = 0;
+    private static Timer timer = new Timer();
 
     public static void createStartingGameInfoView(){
-        BorderPane borderPane = new BorderPane();
-        borderPane.setId("startingGameInfoViewBackground");
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setId("startingGameInfoViewBackground");
 
-        HBox topHbox = new HBox();
-        Label topBorderLabel = new Label("The Oregon Trail");
-        topBorderLabel.setId("topLabelStartingGameInfoView");
-        topHbox.setAlignment(Pos.CENTER);
-        topHbox.getChildren().add(topBorderLabel);
-        borderPane.setTop(topHbox);
+        gridPane.add(canvas, 0, 0);
 
-        HBox bottomHbox = new HBox();
-        Label bottomBorderLabel = new Label("---Press any key to continue---");
-        bottomBorderLabel.setId("bottomLabelStartingGameInfoView");
-        bottomHbox.setAlignment(Pos.CENTER);
-        bottomHbox.getChildren().add(bottomBorderLabel);
-        borderPane.setBottom(bottomHbox);
+        gc.setFill(Color.YELLOW);
+        gc.setFont(new Font(20));
+        gc.fillText("The Oregon Trail", (Main.getPrimaryStage().getWidth()/2)-80 ,15);
+        gc.fillText("---Press any button to continue---", (Main.getPrimaryStage().getWidth()/2)-170 ,Main.getPrimaryStage().getHeight()-400);
 
-        borderPane.setCenter(canvas);
+        timer.schedule(new scrollTextTimer(), 0, 35);
 
-        Timer timer = new Timer();
-        timer.schedule(new test(), 0, 100);
-
-        Scene scene = new Scene(borderPane, Main.getPrimaryStage().getScene().getWidth(), Main.getPrimaryStage().getScene().getHeight());
+        Scene scene = new Scene(gridPane, Main.getPrimaryStage().getScene().getWidth(), Main.getPrimaryStage().getScene().getHeight());
 
         scene.getStylesheets().add("resources/main.css");
 
@@ -56,25 +51,56 @@ public class StartingGameInfoView {
             timer.cancel();
         });
 
+        String path = "starWars.mp3";
+        Media media = new Media(new File(path).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
+        MediaView mediaView = new MediaView(mediaPlayer);
+
+
         Main.getPrimaryStage().setScene(scene);
     }
 
-    public static void testMe(){
+    public static void scrollText(){
+        double height = Main.getPrimaryStage().getHeight();
+        double width = Main.getPrimaryStage().getWidth();
         gc.setFill(Color.YELLOW);
-        gc.setStroke(Color.YELLOW);
-        gc.clearRect(10+x, 60+x, 30+x, 30+x);
-        gc.fillOval(10+x, 60+x, 30+x, 30+x);
+        gc.setFont(new Font(20));
         x++;
-        //gc.clearRect(10,60,30,30);
+        gc.clearRect(0, height-x-10, width, 500);
+        gc.fillText("Welcome to Oregon Trail Remade!\n" +
+                "The Oregon Trail is a 2,200-mile\n" +
+                "historic east–west large-wheeled\n" +
+                "wagon route and emigrant trail that\n" +
+                "connected the Missouri River to valleys\n" +
+                "in Oregon. \n" +
+                "This game was created in the trails\n" +
+                "memory.  The trail is a long and\n" +
+                "treacherous one and for some reason\n" +
+                "you decided the \"Grass is greener\n" +
+                "on the other side.\" You also decided\n" +
+                "to take your spouse and three children\n" +
+                "with you(Man you're smart...). Make sure\n" +
+                "to plan accordingly and make smart\n" +
+                "decisions throughout the trail or\n" +
+                "it might end up costing you or your\n" +
+                "families life.\n" +
+                "Anyways....I hope you enjoy the game\n" +
+                "and good luck ;)\n" , (width/2)-160 ,height+10-x);
+
+        //Execution finished code
+        if(height+30-x <= -450){
+            gc.fillText("---Press any button to continue---", (width/2)-170 ,height-400);
+            timer.cancel();
+        }
     }
     public static void setStartingGameInfoView(Scene scene){StartingGameInfoView.startingGameInfoView = scene;}
     public static Scene getStartingGameInfoView(){ return StartingGameInfoView.startingGameInfoView;}
 }
 
-class test extends TimerTask{
-
+class scrollTextTimer extends TimerTask{
     @Override
     public void run() {
-        StartingGameInfoView.testMe();
+        StartingGameInfoView.scrollText();
     }
 }

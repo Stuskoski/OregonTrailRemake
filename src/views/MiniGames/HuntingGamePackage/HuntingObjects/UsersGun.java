bcf420d3306a1b5826c320abec23c6e8e225f409
Stuskoski <Stuskoski@yahoo.com>
2016-03-08 15:13:43 -0600

    drawing gun and removing gun working. Considering adding multiple layers with canvases

--- src/views/MiniGames/HuntingGamePackage/HuntingObjects/UsersGun.java
+++ src/views/MiniGames/HuntingGamePackage/HuntingObjects/UsersGun.java
@@ -5,9 +5,7 @@ import items.ItemInterface;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.transform.Rotate;
-import main.Main;
-import sun.java2d.pipe.DrawImage;
+import views.MiniGames.HuntingGamePackage.HuntingGame;
 
 import java.awt.*;
 
@@ -16,6 +14,9 @@ import java.awt.*;
  * Creates the users gun and gets
  * all the properties to be used
  * in the hunting mini game
+ *
+ * Can have multiple canvas for layers
+ * to only erase certain items.
  */
 public class UsersGun {
     private static GraphicsContext localGc;
@@ -25,24 +26,30 @@ public class UsersGun {
         switch (gun){
             case "Alien Rifle":{
                 userGun = new AlienRifle(1);
+                gc.clearRect((HuntingGame.getScene().getWidth()/2) - 100, HuntingGame.getScene().getHeight()-150, 225, 175);
                 break;
             }
             case "Flintlock Pistol":{
                 userGun = new FlintlockPistol(1);
+                gc.clearRect((HuntingGame.getScene().getWidth()/2) - 100, HuntingGame.getScene().getHeight()-150, 225, 175);
                 break;
             }
             case "Musket":{
                 userGun = new Musket(1);
+                gc.fill();
+                gc.clearRect((HuntingGame.getScene().getWidth()/2) - 100, HuntingGame.getScene().getHeight()-150, 225, 175);
                 break;
             }
             case "Rifle":{
                 userGun = new Rifle(1);
+                gc.clearRect((HuntingGame.getScene().getWidth()/2) - 100, HuntingGame.getScene().getHeight()-150, 225, 175);
                 break;
             }
             case "Shotgun":{
                 userGun = new ShotGun(1);
+                gc.clearRect((HuntingGame.getScene().getWidth()/2) - 100, HuntingGame.getScene().getHeight()-150, 225, 175);
                 Image image = new Image("resources/HuntingMiniGame/shotgun.png");
-                gc.drawImage(image, (Main.getPrimaryStage().getScene().getWidth()/2)-30, Main.getPrimaryStage().getScene().getHeight()-125);
+                gc.drawImage(image, (HuntingGame.getScene().getWidth()/2) - 30, HuntingGame.getScene().getHeight()-125);
                 break;
             }
         }
@@ -53,8 +60,8 @@ public class UsersGun {
 
         createScene.setOnMouseMoved(event -> {
             System.out.println(event.getX() + " " + event.getY());
-            Rotate r = new Rotate(45, event.getX(), event.getY());
-            localGc.setTransform(r.getMxx(), r.getMyx(), r.getMxy(), r.getMyy(), r.getTx(), r.getTy());
+            //Rotate r = new Rotate(45, event.getX(), event.getY());
+            //localGc.setTransform(r.getMxx(), r.getMyx(), r.getMxy(), r.getMyy(), r.getTx(), r.getTy());
         });
 
     }

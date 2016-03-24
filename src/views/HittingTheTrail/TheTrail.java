0add4589d62568d8789d5538ad7585663415c370
Stuskoski <Stuskoski@yahoo.com>
2016-03-23 21:34:25 -0500

    starting to work on trail scene

--- src/views/HittingTheTrail/TheTrail.java
+++ src/views/HittingTheTrail/TheTrail.java
@@ -2,6 +2,7 @@ package views.HittingTheTrail;
 
 import Start.Main;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 /**
@@ -15,7 +16,22 @@ public class TheTrail {
         GridPane gridPane = new GridPane();
         gridPane.setId("TheTrailGridPaneMoving");
 
+        Button switchBack = new Button("Switch");
+        Button switchBack2 = new Button("Switch2");
+
+        gridPane.add(switchBack, 1, 1);
+        gridPane.add(switchBack2, 2, 2);
+
+        switchBack.setOnAction(event -> {
+            gridPane.setId("TheTrailGridPaneMoving");
+        });
+        switchBack2.setOnAction(event -> {
+            gridPane.setId("mainScreen");
+        });
+
+
         scene = new Scene(gridPane, Main.getPrimaryStage().getScene().getWidth(), Main.getPrimaryStage().getScene().getHeight());
+        scene.getStylesheets().addAll("resources/main.css");
 
         Main.getPrimaryStage().setScene(scene);
     }
